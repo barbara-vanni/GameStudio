355d5a29993afb831cd392dd29c94219612ecee3
OroitzLR <oroitzlr@gmail.com>
2023-07-05 15:20:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#Salut
+import pygame
+from pygame.locals import QUIT
+
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption('Basic Pygame program')
+
+
+# Initialise screen
+while True :
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
