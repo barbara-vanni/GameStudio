6d4152a266184b950ba09c3a89b8c3c345b45be7
OroitzLR <oroitzlr@gmail.com>
2023-07-10 00:16:31 +0200

    cleaned raycasting

--- poubelle.py
+++ poubelle.py
@@ -1,8 +1,8 @@
 import pygame as pg
 import numpy as np
-from Settings import *
+from settings import *
 
-def raycasting(window, map): 
+def raycassting(window, map): 
     angle_init = player_angle - HALF_FOV
     start_x = 0
     for ray in range(NUM_RAYS):
@@ -12,20 +12,20 @@ def raycasting(window, map):
         cos_a = np.cos(angle_init)
         sin_a = sin_a if sin_a else 0.000001
         cos_a = cos_a if cos_a else 0.000001
-        vector_dir = (pg.math.Vector2(sin_a*velocity/ECHELLE, cos_a*velocity/ECHELLE))
+        vector_dir = (pg.math.Vector2(sin_a*velocity/ECHELLE, cos_a*velocity/ECHELLE)) #erreur
         hit = False
         dist = 0 
         while not hit: 
             vector_init += vector_dir # rayon lancé
             if map[int(vector_init.y)][int(vector_init.x)] == 1:
                 hit = True
-            dist += velocity #rayon à touché = distance parcourue
+            dist += velocity #rayon à touché = distance parcourue       ERREUR
         projected_height = int(WALL_HEIGHT / dist)
         half_projected_height = projected_height // 2
         start_y = HALF_HEIGHT - half_projected_height
         print(start_y)
         start_x += SCALE
-        pg.draw.rect(window, (200,200,200), (start_x, start_y, SCALE, projected_height))
+        pg.draw.rect(window, (100,200,100), (start_x, start_y, SCALE, projected_height))
 
     
     
--- raycasting.py
+++ raycasting.py
@@ -2,3 +2,25 @@ import pygame as pg
 import numpy as np
 from settings import *
 
+def raycasting(window, map):
+    ray_angle = player_angle - HALF_FOV
+    start_x = 0
+    for ray in range(NUM_RAYS):
+        ray_vector = pg.Vector2(player_pos.x, player_pos.y)
+        sin_a = np.sin(ray_angle)
+        cos_a = np.cos(ray_angle)
+        sin_a = sin_a if sin_a else 0.000001
+        cos_a = cos_a if cos_a else 0.000001
+        direction_vector = pg.Vector2(sin_a * RAY_SPEED, cos_a * RAY_SPEED)
+        hit = False
+        distance = 0
+        while not hit:
+            ray_vector += direction_vector
+            distance += RAY_SPEED
+            if map[int(ray_vector.y)][int(ray_vector.x)] == 1:
+                hit = True
+        projected_height = int(WALL_HEIGHT / distance)    
+        half_projected_height = projected_height // 2
+        start_y = HALF_HEIGHT - half_projected_height
+        start_x += SCALE
+        pg.draw.rect(window, (200,200,200), (start_x, start_y, SCALE, projected_height))
