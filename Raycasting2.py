15fb297c33d702598f41840646db5822efdf8b61
Dhjali <barbara.vanni@laplateforme.io>
2023-07-10 16:50:09 +0200

    Ca bouge enfin !

--- Raycasting2.py
+++ Raycasting2.py
@@ -1,13 +1,13 @@
 import pygame as pg
 import numpy as np
 from settings import *
+from player import *
 
-def raycasting(window, map): 
-    angle_init = player_angle - HALF_FOV
+def raycasting(window, map, player): 
+    angle_init = player.angle - HALF_FOV
     start_x = 0
     for ray in range(NUM_RAYS):
-        vector_init = pg.math.Vector2(player_pos.x, player_pos.y)
-        # vector_init = player_pos #px et py point de départ du rayon 
+        vector_init = pg.math.Vector2(player.position.x, player.position.y)  # vector_init = player_pos #px et py point de départ du rayon 
         sin_a = np.sin(angle_init)      #direction du rayon
         cos_a = np.cos(angle_init)
         sin_a = sin_a if sin_a else 0.000001
@@ -15,16 +15,18 @@ def raycasting(window, map):
         vector_dir = (pg.math.Vector2(sin_a*RAY_SPEED, cos_a*RAY_SPEED))
         hit = False
         dist = 0 
+        # print(vector_init)
         while not hit: 
             vector_init += vector_dir # rayon lancé
             if map[int(vector_init.y)][int(vector_init.x)] == 1:
                 hit = True
-            dist += RAY_SPEED * 100 #rayon à touché = distance parcourue
-        projected_height = int(WALL_HEIGHT / dist)
+            dist += RAY_SPEED #rayon à touché = distance parcourue
+        projected_height = int(RES_Y / dist)
         half_projected_height = projected_height // 2
         start_y = HALF_HEIGHT - half_projected_height
         start_x += SCALE
         pg.draw.rect(window, (200,200,200), (start_x, start_y, SCALE, projected_height))
+        angle_init += DELTA_ANGLE
 
     
     
--- Settings.py
+++ Settings.py
@@ -9,7 +9,7 @@ HALF_HEIGHT = RES_Y // 2
 ECHELLE = 100 
 
 # player settings
-PLAYER_POS_INIT = pg.math.Vector2(8,11) # position initial
+PLAYER_POS_INIT = pg.math.Vector2(8,11) # position initiale
 player_pos = PLAYER_POS_INIT
 player_angle = 0.0
 RAY_SPEED = 0.04  #
@@ -19,7 +19,7 @@ PLAYER_SPEED = 0.02
 # ray casting settings
 FOV = np.pi / 3  # Approx 60 Degrees
 HALF_FOV = FOV / 2
-NUM_RAYS = 80
+NUM_RAYS = 800
 MAX_DEPTH = 2000
 DELTA_ANGLE = FOV / NUM_RAYS
 DIST = NUM_RAYS / (2 * np.tan(HALF_FOV))
