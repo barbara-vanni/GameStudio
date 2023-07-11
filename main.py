9e3e4a4c9f404e425b3df8b63751789d855c8fca
Dhjali <barbara.vanni@laplateforme.io>
2023-07-11 09:28:45 +0200

    mise à zero

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-import pygame
-import sys
-from pygame.locals import QUIT
-pygame.init()
 
-# Definition ecran
-size = 800,600
-screen = pygame.display.set_mode(size)
-
-
-# Sortie et fin de jeu
-while True :
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
 
