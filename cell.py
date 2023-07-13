c96b762e718ae32cff127060a8a9095d1c861768
Dhjali <barbara.vanni@laplateforme.io>
2023-07-13 11:34:55 +0200

    jsp

--- cell.py
+++ cell.py
@@ -1,5 +1,6 @@
 from settings import *
 from enum import Enum
+import pygame as pg
 
 
 class Cell_type(Enum) : 
@@ -26,6 +27,8 @@ class Floor(Cell) :
     type = Cell_type.FLOOR
     def __init__(self, params) : 
         super().__init__(params)
-        
+        self.color = colors[0]
+        if len(params) > 0 :
+            self.color = colors[int(params[0])]  
 
 
